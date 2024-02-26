42777c3c6ba989d3e7b351f56d58c74791fde4eb
Lekkk02 <maikel.g972@gmail.com>
2024-02-26 16:17:41 -0400

    Initial commit

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,15 @@
+const purgecss = [
+  "@fullhuman/postcss-purgecss",
+  {
+    content: [
+      "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    whitelistPatterns: [/^slick-/],
+    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+  },
+];
 module.exports = {
   plugins: {
     tailwindcss: {},
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,49 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    boxShadow: {
+      sm: "0 1px 2px 0 rgba(0, 0, 0, 0.05)",
+      DEFAULT:
+        "0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)",
+      md: "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
+      lg: "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
+      xl: "0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)",
+      t: "0 -1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)",
+      orange: "0px 20px 20px -15px rgba(14,18,59,0.81) ",
+      "orange-md": "0px 20px 40px -15px rgba(14,18,59,0.81) ",
+      none: "none",
+    },
+    colors: {
+      transparent: "transparent",
+      black: {
+        500: "#4F5665",
+        600: "#0B132A",
       },
+      orange: {
+        100: "#ececff",
+        500: "#0e123b",
+      },
+      green: {
+        500: "#2FAB73",
+      },
+      white: {
+        300: "#F8F8F8",
+        500: "#fff",
+      },
+      purple: {
+        500: "#0e123b",
+      },
+      gray: {
+        100: "#EEEFF2",
+        400: "#AFB5C0",
+        500: "#DDDDDD",
+      },
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {
+      boxShadow: ["active", "hover"],
     },
   },
   plugins: [],
